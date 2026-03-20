de8d920d1085b1229e11ba5319b16e4e7cab8d3b
rbarr1983 <rbarr1983@gmail.com>
2026-03-20 15:22:02 +0000

    removed counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Treatments from "./pages/Treatments"
 import About from "./pages/About"
 import Footer from "./components/Footer"
 import ScrollToTop from "./components/ScrollToTop"
-import { Analytics } from "@vercel/analytics/next"
+
 
 function App() {
   return (
@@ -20,8 +20,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/treatments" element={<Treatments />} />
          <Route path="/about" element={<About />} />
-         
-<Analytics />
+
       </Routes>
        
 <Footer /> 
